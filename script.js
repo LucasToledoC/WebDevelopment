717b42ffbe4379d4175e81c4b173a0b7ac62c459
Lucas Toledo Cortonezi <lucastoledocort25@gmail.com>
2025-03-27 09:13:07 -0300

    tentando refazer o commit

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ var goat = prompt("qual é o goat do basquete?");
 var jersey = prompt(`qual numero o ${goat} usa na regata?`);
 
 alert(`o seu goat no basquete é o ${goat}? boa escolha, e ele usa o numero ${jersey}? que interessante.`);
+//teste
