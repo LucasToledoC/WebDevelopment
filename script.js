69d8de438dde454c1648b4a938a3e7e8aad99076
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-24 09:51:21 -0300

    ultimos exemplos da aula

--- script.js
+++ script.js
@@ -56,21 +56,36 @@
 // }
 
 // console.log(calculadora(10, 5, "+"))
-var n1 = null
-var n2 = null
+// var n1 = null
+// var n2 = null
 
-function subtracao(n1, n2) {
-    var n1 = parseInt(prompt("me diga um número"))
-    var n2 = parseInt(prompt("me diga um outro número"))
+// function subtracao(n1, n2) {
+//     var n1 = parseInt(prompt("me diga um número"))
+//     var n2 = parseInt(prompt("me diga um outro número"))
 
 
-    if (n1 > n2) {
-        resultado = n1 - n2
-        console.log(resultado)
-    }
-    else {
-        resultado = n2 - n1
-        console.log(resultado)
-    }
-}
-subtracao(n1, n2)
+//     if (n1 > n2) {
+//         resultado = n1 - n2
+//         console.log(resultado)
+//     }
+//     else {
+//         resultado = n2 - n1
+//         console.log(resultado)
+//     }
+// }
+// subtracao(n1, n2)
+
+
+// const soma = function (a, b) { return a + b }
+// const subtrair = function (a, b) { return a - b }
+
+// console.log(soma(15, 20))
+// console.log(subtrair(15, 5))
+
+
+// const soma = (a, b) => { return a + b }
+
+// const subtrair = (a, b) => a - b;
+
+// console.log(soma(15, 20))
+// console.log(subtrair(20, 5))
