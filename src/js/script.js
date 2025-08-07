c797cf1c41d45ed32b4ec60e60954134f6eb151c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-07 09:39:20 -0300

    aula do dia 07 do 08

--- src/js/script.js
+++ src/js/script.js
@@ -1,83 +1,42 @@
-// const meuArray = ['rosa', 'verde', 'azul'];
-// console.log(meuArray.length);
+// localStorage.setItem('boas-vindas', 'olá, Dev!')
+// localStorage.setItem('valido', true)
+// localStorage.setItem('numero', 20)
 
-// for (let i = 0; i < meuArray.length; i++) {
-//     console.log(meuArray[i]);
+// let mensagem = localStorage.getItem('boas-vindas')
+// let tipo = localStorage.getItem('valido')
+// let numero = localStorage.getItem('numero')
+
+// console.log(mensagem);
+// console.log(tipo);
+// console.log(numero);
+
+// session storage
+
+
+// sessionStorage.setItem('selecionados', [1,2,3]);
+// sessionStorage.setItem('valido', false);
+// sessionStorage.setItem('email', 'info@gmail.com');
+
+// let lista = sessionStorage.getItem('selecionados').split(",");
+// let tipo = sessionStorage.getItem('valido') == 'true';
+// let email = sessionStorage.getItem('email')
+
+// console.log(lista)
+// console.log(tipo)
+// console.log(email)
+
+
+// for (let i = 0; i < localStorage.length; i++) {
+//     let chave = localStorage.key(i);
+//     console.log("Chave: " + chave + "   Valor:  " +
+// localStorage.getItem(chave));
 // }
 
-//.push // no final
-//unshift // no inicio
+// localStorage.setItem('boas-vindas', 'Olá, Dev');
+// sessionStorage.setItem('valido', true);
 
-// const nomes = ['Luis', 'Ana', "Joao", 'Julia']
-// console.log(nomes)
+// localStorage.removeItem('boas-vindas')
+// sessionStorage.removeItem('valido')
 
-// nomes.pop();
-// console.log(nomes)
-
-// nomes.shift();
-// console.log(nomes)
-
-// const nomes = ["Rita", "Mara", "Luiz", "Ana", "Léo", "Jão", "Marco"]
-// nomes.splice(2, 4)
-
-// console.log(nomes)
-
-// const nomes = ['Jordan', 'Lebron', 'Kobe', 'Alemidia']
-// console.log(nomes.join(", "))
-
-// console.log(nomes.join("*"))
-
-// const caes = ["pupy", "Ronnied"]
-// const gatos = ["Mishi", 'Garfield', "Zuri"]
-
-// const animais = caes.concat(gatos)
-// console.log(animais)
-
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
-
-// const masculinos = nomes.slice(1, 3);
-// Pedro, Miguel
-
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
-
-// console.log(nomes.includes('Rita'))
-// console.log(nomes.includes('Miguel'))
-// console.log(nomes.includes('Julieta'))
-
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
-
-// console.log(nomes.indexOf('Rita'))
-// console.log(nomes.indexOf('Ana'))
-// console.log(nomes.indexOf('Julieta'))
-
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
-
-// nomes.reverse()
-// console.log(nomes)
-
-// const listaNomes = []
-// let quantidade = 5
-
-// do {
-//     let entrada = prompt("inserir nome")
-//     listaNomes.push(entrada.toUpperCase())
-//     console.log(listaNomes.length)
-// } while (listaNomes.length != quantidade)
-
-// const novaLista = listaNomes.concat(["ANA", "EMA"])
-
-// console.log(novaLista.join("\n"))
-
-cores = []
-quantidade = 5
-do {
-    cor1 = prompt("diga uma cor")
-    if (!isNaN(cor1)) {
-        cores.push(cor1)
-        console.log(cores)
-        quantidade++
-    }
-    else {
-        cor1 = prompt("diga uma cor")
-    }
-} while (quantidade < 5)
+// localStorage.clear
+// sessionStorage.clear
