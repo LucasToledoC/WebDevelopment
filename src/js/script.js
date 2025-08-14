1a2618f9915f9acdc2de5b14d213752ac670613a
Lucas Toledo Cortonezi <lucastoledocort25@gmail.com>
2025-08-14 10:17:00 -0300

    aula 1408

--- src/js/script.js
+++ src/js/script.js
@@ -1,83 +1,138 @@
-// const meuArray = ['rosa', 'verde', 'azul'];
-// console.log(meuArray.length);
 
-// for (let i = 0; i < meuArray.length; i++) {
-//     console.log(meuArray[i]);
+// const btn = document.getElementById('botao')
+// const popup = document.getElementById('popup')
+
+// btn.addEventListener('click', () => {
+//     popup.classList.add('popup-active')
+
+//     setTimeout(() =>
+//     {
+//         popup.classList.remove('popup-active')
+//     }, 2500)
+// })
+
+// for (let letra of "Olá") {
+//     console.log(letra)
+// }
+// for (let letra of "Mundo") {
+//     console.log(letra)
 // }
 
-//.push // no final
-//unshift // no inicio
+// for (let letra of "olá ") {
+//     setTimeout(() => {
+//         console.log(letra)
+//     }, 1000)
+// }
 
-// const nomes = ['Luis', 'Ana', "Joao", 'Julia']
-// console.log(nomes)
+// for (let letra of "mundo") {
+//     setTimeout(() => {
+//         console.log(letra)
+//     }, 3000)
+// }
 
-// nomes.pop();
-// console.log(nomes)
+// function multiply(x, y) {
+//     return x * y;
+// }
 
-// nomes.shift();
-// console.log(nomes)
+// function printSquare(x) {
+//     let s = multiply(x, x);
+//     console.log(s);
+// }
 
-// const nomes = ["Rita", "Mara", "Luiz", "Ana", "Léo", "Jão", "Marco"]
-// nomes.splice(2, 4)
+// printSquare(3)
 
-// console.log(nomes)
+// console.log('1 - Inicio do processo')
 
-// const nomes = ['Jordan', 'Lebron', 'Kobe', 'Alemidia']
-// console.log(nomes.join(", "))
+// setTimeout(() => {
+//     console.log("2 - Meio do processo")
+// }, 0)
 
-// console.log(nomes.join("*"))
+// console.log("3 - Fim do processo")
 
-// const caes = ["pupy", "Ronnied"]
-// const gatos = ["Mishi", 'Garfield', "Zuri"]
+// setInterval(() => {
+//     console.log("Tic")
+// }, 10000)
 
-// const animais = caes.concat(gatos)
-// console.log(animais)
+// let counter = 0
+// const interval = setInterval(() => {
+//     counter++
+//     console.log("Counter: ", counter)
 
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
+//     if(counter >= 5) {
+//         clearInterval(interval)
+//         console.log("O intervalo foi removido")
+//     }
+//  }, 1000)
 
-// const masculinos = nomes.slice(1, 3);
-// Pedro, Miguel
+// console.log("Inicio")
 
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
+// const fim = setTimeout(() => {
+//     console.log("Fim")
+// }, 2000)
 
-// console.log(nomes.includes('Rita'))
-// console.log(nomes.includes('Miguel'))
-// console.log(nomes.includes('Julieta'))
+// clearTimeout(fim)
 
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
+// new Promise((resolve, reject ) => {
+//     //corpo da promessa
+// })
 
-// console.log(nomes.indexOf('Rita'))
-// console.log(nomes.indexOf('Ana'))
-// console.log(nomes.indexOf('Julieta'))
+// const eventoFuturo = () => {
+//     return new Promise((resolve, reject) => {
+//         //corpo da promessa
+//     })
+// }
+// console.log(eventoFuturo())
+// //promise pendente
 
-// const nomes = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanessa'];
+// const eventoFuturo = (res) => {
+//     return new Promise((resolve, reject) => {
+//         if (res === true) {
+//             resolve('Promessa resolvida')
+//         } else {
+//             reject('Promessa rejeitada')
+//         }
+//     })
+// }
 
-// nomes.reverse()
-// console.log(nomes)
+// console.log(eventoFuturo(true))
+// //promise {fulfilled promessa resolvida}
 
-// const listaNomes = []
-// let quantidade = 5
+// console.log(eventoFuturo(false))
+// //promise {rejected promessa rejeitada}
 
-// do {
-//     let entrada = prompt("inserir nome")
-//     listaNomes.push(entrada.toUpperCase())
-//     console.log(listaNomes.length)
-// } while (listaNomes.length != quantidade)
+// const eventoFuturo = (res) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             res ? resolve('Promessa resolvida') : reject('promessa rejeitada')
+            
+//         }, 2000)
+//     })
+// }
 
-// const novaLista = listaNomes.concat(["ANA", "EMA"])
+// console.log(eventoFuturo(true))
 
-// console.log(novaLista.join("\n"))
+// console.log(eventoFuturo(false))
 
-cores = []
-quantidade = 5
-do {
-    cor1 = prompt("diga uma cor")
-    if (!isNaN(cor1)) {
-        cores.push(cor1)
-        console.log(cores)
-        quantidade++
-    }
-    else {
-        cor1 = prompt("diga uma cor")
-    }
-} while (quantidade < 5)
+const BD = [
+    { id: 1, nome: 'Produto 1', preco : 1500},
+    { id: 2, nome: 'Produto 2', preco : 2500},
+    { id: 3, nome: 'Produto 3', preco : 3500}
+]
+const pedirProdutos = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(BD)
+        }, 5000)
+    })
+}
+let produtos = []
+
+const renderProdutos = (arr) => {
+    console.log(produtos)
+}
+
+pedirProdutos()
+    .then((res) => {
+        produtos = res
+        renderProdutos(produtos)
+    })
